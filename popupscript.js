b1a9cc16d51aa7a16cddab46c6ea332222de79a8
premiumkwame <kransly007@gmail.com>
2026-03-25 12:51:28 +0000

    commit

--- popupscript.js
+++ popupscript.js
@@ -1,17 +1,17 @@
-// show after 3 seconds
+// show after 5 seconds
 setTimeout(function(){
     document.getElementById("promo").style.display = "flex";
-}, 3000);
+}, 5000);
 
 // close function
 function closePromo() {
     document.getElementById("promo").style.display = "none";
 }
 
-// show popup after 8 seconds
+// show popup after 15 seconds
 setTimeout(function(){
     document.getElementById("mc_embed_shell").style.display ="flex"
-},8000);
+},15000);
 
 // close popup when X is clicked
 document.querySelector(".close-btn").addEventListener("click",function(){
